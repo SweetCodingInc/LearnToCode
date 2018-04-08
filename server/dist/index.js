39dcb7f81580f649a2b3502b5468f807f3323e1f
Aditya <contact.adityaparab@gmail.com>
2018-04-08 14:31:37 +0530

    Added database seeding capabilities

--- server/dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var learn_to_code_1 = require("./server/learn-to-code");
-var Learn2Code = new learn_to_code_1.LearnToCode();
-Learn2Code
-    .configure()
-    .start()
-    .then(console.log)
-    .catch(console.error);
